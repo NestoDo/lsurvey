a1c8aa19eef31c74bbeddf9b96d426d94e3199fd
NestoDo <nesto.do@gmail.com>
2021-02-14 20:11:23 -0600

    Set DBContext

---
+++ Api/SL.Survey.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SL.Survey.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
